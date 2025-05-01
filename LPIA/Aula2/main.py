d36c6e4006e0577dea4d69c49799dc62f1130417
Lucas <lucas.2020109662@unicap.br>
2025-05-01 18:33:33 -0300

    First Commit

---
+++ LPIA/Aula2/main.py
@@ -0,0 +1,43 @@
+# #Atv 1
+# age1 = int(input("Digite uma idade"))
+# age2 = int(input("Digite outra idade"))
+
+# if (age1 > age2):
+#     print(f"A idade {age1} é maior que {age2}")
+# elif (age1 == age2):
+#     print(f"As idades são iguais")
+# else:
+#     print(f"A idade {age2} é maior que {age1}")
+
+
+# Atv 2
+# str1 = (input("Digite uma string: "))
+# str2 = (input("Digite outra string: "))
+
+# if (str1 == str2):
+#     print(f"São iguais")
+# else:
+#     print(f"Não são iguais")
+# 
+# # #Atv 3
+# age = int(input("Digite a sua idade: "))
+# if age >= 18:
+#     carteira = input("Você tem carteira de habilitação? (Sim/Não): ")
+#     if carteira == "Sim":
+#         print(f"Você tem {age} anos e possui carteira de habilitação")
+#     elif carteira == "Não":
+#         print(f"Você tem {age} anos e não possui carteira de habilitação")
+#     else:
+#          print("Entrada inválida")
+# elif age < 18:
+#     print(f"Você tem {age} anos e não possui carteira de habilitação")
+# else:
+#     print("Entrada inválida")
+
+#Atv 9
+num = int(input("Digite um número: "))
+
+if ((num % 2) == 0):
+    print(f"O número {num} é par")
+else:
+    print(f"O número {num} é ímpar")
