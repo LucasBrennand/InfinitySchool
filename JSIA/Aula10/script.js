b216d46c7aef496feb21f026569b9c61f8375bdb
Lucas <lucas.2020109662@unicap.br>
2025-06-27 00:58:47 -0300

    Aula 10

---
+++ JSIA/Aula10/script.js
@@ -0,0 +1,45 @@
+//Atividade 2
+console.log("Atividade 2")
+const paragrafo = document.getElementsByTagName("p")
+for(i of paragrafo){
+    i.innerHTML = "texto alterado"
+}
+
+//Atividade 3
+console.log("Atividade 3")
+const box = document.querySelectorAll(".box")
+for(i of box){
+    i.style = "background-color: blue"
+}
+
+//Atividade 4
+console.log("Atividade 4")
+const minhaImagem = document.getElementById("minhaImagem")
+const meuLink = document.getElementById("meuLink")
+mudarLinks = () =>{
+    minhaImagem.setAttribute("src", "https://i.ytimg.com/vi/DQnVWG7jUXs/maxresdefault.jpg")
+    meuLink.setAttribute("href", "https://i.ytimg.com/vi/DQnVWG7jUXs/maxresdefault.jpg")
+}
+
+//Atividade 5
+console.log("Atividade 5")
+const container = document.getElementById("container")
+const botaoAdd = document.getElementById("adicionarParagrafo")
+const botaoRemove = document.getElementById("removerParagrafo")
+
+let arrayP = []
+botaoAdd.addEventListener("click", ()=>{
+    let novoP = document.createElement("p")
+    novoP.innerHTML = "lorem25"
+    container.appendChild(novoP)
+    console.log(novoP)
+
+    
+})
+
+botaoRemove.addEventListener("click", ()=>{
+    const p = document.getElementsByTagName("p")
+    if(container.lastChild.contains("<p>")){
+        container.removeChild(container.lastChild)
+    }
+})
---
+++ JSIA/Aula9/script.js
@@ -0,0 +1,15 @@
+//Atividade 4
+console.log("Atividade 4")
+
+soma = (a,b) => a + b
+console.log(soma(2,4))
+
+//Atividade 5
+console.log("Atividade 5")
+const numeros = [1, 2, 3, 4, 5]
+numerosQuadrados = numeros.map(valor => valor ** 2)
+console.log(numerosQuadrados)
+
+//Atividade 6
+console.log("Atividade 6")
+
