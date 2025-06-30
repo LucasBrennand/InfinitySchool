8590ceae74e07c1b9d5330431c916bdde593fb62
Lucas Brennand <100360881+LucasBrennand@users.noreply.github.com>
2025-06-30 20:36:54 -0300

    Update script.js

--- JSIA/Aula12/script.js
+++ JSIA/Aula12/script.js
@@ -1,22 +1,68 @@
 //Atividade 1
 console.log("Atividade 1")
 
-const enviar = document.getElementById("enviar")
-const nomeInput = document.getElementById("nome-input")
-const para = document.getElementById("para")
+const nomeInput = document.querySelector("#nomeInput")
+const enviar = document.querySelector("#enviar")
+const div1 = document.querySelector(".atividade1")
+const lista = document.querySelector("#lista")
+const remove = document.querySelector("#remover")
+const clear = document.querySelector("#clear")
+const temaSelect = document.querySelector("#temaSelect")
+const formBtn = document.querySelector("#formBtn")
 
-const nomeLista = []
+let local = JSON.parse(localStorage.getItem("Nome")) || []
+for(let i of local){
+const itemAntigo = document.createElement("li")
+lista.appendChild(itemAntigo)
+itemAntigo.innerHTML = i
+console.log(i)
+}
+
+let preferenciasLocal = JSON.parse(localStorage.getItem("Preferencias")) || []
+console.log(preferenciasLocal)
+
+let preferencias = {
+    tema: "",
+    fonte: ""
+}
+
 enviar.addEventListener("click", (event) => {
-    let nomeInputValue = nomeInput.value
-    let li = document.createElement("li")
-    para.appendChild(li)
-    li.textContent = nomeInputValue
-    nomeLista.push(nomeInputValue)
-    localStorage.setItem("nome", JSON.stringify(nomeLista))
+    const item = document.createElement("li")
+    const nomeInputValue = nomeInput.value
+    lista.appendChild(item)
+    item.innerHTML = nomeInputValue
+    local.push(nomeInputValue)
+    console.log(local)
+    localStorage.setItem("Nome", JSON.stringify(local))
 })
-lista = JSON.parse(localStorage.getItem("nome"))
-console.log(lista)
-items = document.createElement("li")
-items.textContent = JSON.parse(localStorage.getItem("nome") )
-para.appendChild(items)
-console.log(items)
+
+remove.addEventListener("click", (event) => {
+    const ultimoItem = lista.lastChild
+    lista.removeChild(ultimoItem)
+    local.pop()
+    localStorage.setItem("Nome", JSON.stringify(local))
+    console.log(local)
+})
+
+clear.addEventListener("click", (event) => {
+    lista.remove(lista)
+    local = []
+    localStorage.setItem("Nome", JSON.stringify(local))
+    console.log(local)
+})
+
+//Atvidade 4
+console.log("Atividade 4")
+formBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    const radio = document.querySelector(`input[name="fonte"]:checked`)
+    preferencias.tema = temaSelect.value
+    if (radio) {
+        preferencias.fonte = radio.value
+    } else {
+        console.log("Nenhuma fonte foi selecionada.")
+    }
+    localStorage.setItem("Preferencias", JSON.stringify(preferencias))
+    console.log(preferencias)
+})
+
