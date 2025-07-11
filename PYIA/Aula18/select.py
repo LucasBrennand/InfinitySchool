cb93aefc7c265bb26e366d0ea79ab1b1e001483a
Lucas <lucas.2020109662@unicap.br>
2025-07-11 12:35:08 -0300

    projeto

--- PYIA/Aula18/select.py
+++ PYIA/Aula18/select.py
@@ -6,5 +6,4 @@ class Create:
         tabela_input = input("Digite de qual tabela você quer exibir")
         from_input = input("Digite quais colunas você quer exibir ou digite * para todos")
 
-
         "SELECT * FROM PRODUTOS"
