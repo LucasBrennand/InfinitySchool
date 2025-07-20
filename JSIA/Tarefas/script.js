774df870abc8db12b42b636b3e97622991169a02
Lucas <lucas.2020109662@unicap.br>
2025-07-19 23:39:08 -0300

    Finish Tarefas

--- JSIA/Tarefas/script.js
+++ JSIA/Tarefas/script.js
@@ -2,13 +2,27 @@ const username = document.getElementById("username");
 const tarefasBtn = document.getElementById("tarefas-btn");
 const tarefasInput = document.getElementById("tarefas-input");
 const tarefasList = document.getElementById("tarefas-list");
-const tarefaContador = document.querySelector("#tarefa-contador")
+const tarefaContador = document.querySelector("#tarefa-contador");
+const tarefasBtnLimpar = document.querySelector("#tarefas-btn-limpar");
 
 // let namePrompt = prompt("Digite seu nome:")
 // username.textContent = namePrompt;
-const getTarefas = JSON.parse(localStorage.getItem("Tarefas"))
-let tarefasArray = []
+const getTarefas = JSON.parse(localStorage.getItem("Tarefas")) || [];
+let tarefasArray = [];
+if (getTarefas != null) {
+  for (i of getTarefas) {
+    let novoItem = document.createElement("div");
+    novoItem.classList.add("tarefas-item");
+    tarefasList.appendChild(novoItem);
+    novoItem.innerHTML = i;
+    tarefasArray.push(i)
+  }
+  console.log(tarefasArray)
+}
+
+
 tarefasBtn.addEventListener("click", (event) => {
+  event.preventDefault();
   if (tarefasInput.value !== "") {
     let tarefasInputValue = tarefasInput.value;
     let novoItem = document.createElement("div");
@@ -19,11 +33,12 @@ tarefasBtn.addEventListener("click", (event) => {
                                 <button id="tarefas-removerBtn">Remover</button>
                             
     `;
-    tarefasArray.push(novoItem)
-    localStorage.setItem("Tarefas", JSON.stringify(tarefasArray))
-    console.log(tarefasArray)
+    const storageItem = novoItem.innerHTML
+    console.log(novoItem);
+    tarefasArray.push(storageItem);
+    localStorage.setItem("Tarefas", JSON.stringify(tarefasArray));
+    console.log(tarefasArray);
     tarefasInput.value = "";
-    event.preventDefault();
   }
 });
 
@@ -37,6 +52,16 @@ tarefasList.addEventListener("click", (event) => {
     }
   }
   if (event.target.tagName === "BUTTON") {
-    event.target.parentElement.remove();
+    const divItem = event.target.parentElement;
+    const itemHtml = divItem.innerHTML
+    tarefasArray = tarefasArray.filter(item => item != itemHtml);
+    divItem.remove()
+    localStorage.setItem("Tarefas", JSON.stringify(tarefasArray));
   }
+});
+
+tarefasBtnLimpar.addEventListener("click", (event) => {
+  event.preventDefault();
+  localStorage.clear();
+  tarefasList.innerHTML = "";
 });
